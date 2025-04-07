39d5e27966c904fe1822e92246de80cb64051628
Alexisfllv <alexis-fl-live@hotmail.com>
2025-04-07 07:41:50 -0500

    feat(dtos): Creando los request dtos para la reservacion.

---
+++ src/main/java/edu/com/hotelapi/DTO/ReservationRequestDTO.java
@@ -0,0 +1,28 @@
+package edu.com.hotelapi.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationRequestDTO {
+
+    private LocalDateTime resv_start;
+    private LocalDateTime resv_end;
+    private String resv_status;
+    private String user_name;
+    private String user_email;
+    private String user_phone;
+
+    private String bill_name;
+    private String bill_email;
+    private String bill_telephone;
+
+    // fks
+    private Integer user_id;
+    private Integer room_id;
+}
